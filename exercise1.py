b4f060d6a1bd086e2a4c7dc303fc2db94d963171
ASh1ne <77381910+ASh1ne@users.noreply.github.com>
2021-01-23 19:21:13 +0500

    Add files via upload

--- exercise1.py
+++ exercise1.py
@@ -1,12 +1,4 @@
-a = 10
-sayHi = "Hello, World"
-print(sayHi+" "+str(a))
-print(a)
-print("Ели ли вы овощи сегодня")
-answer = input()
-if(answer == 'да'):
-    print("Сколько овощей?")
-    times = int(input())
-    print(f"Товарищ ел овощи в количестве {times} шт.")
-else:
-    print("Очень плохо. Ешьте овощи")
+defaultList = [1,7,'mama',8.4]
+for element in defaultList:
+    print(type(element))
+    
--- exercise6.py
+++ exercise6.py
@@ -1,11 +1,29 @@
-dayCount = 0
-print("Введите начальную дистанцию")
-defaultDistance = int(input())
-print("Введите необходимый результат")
-upgradeDistance = int(input())
+goods = []
+i = 0
+exit = ''
+print('После окнчания ввода для продолжения обработки ведите "вых" в параметр "название"')
+while True:
+    goodCard = {'название': '', 'цена': '', 'количество': '', 'ед.': ''}
+    num = i+1
+    for characteristicName in goodCard.keys():
+        paramName = input(f'Введите параметр {characteristicName}: ')
+        if paramName == 'вых':
+            exit = paramName
+            break
+        goodCard[characteristicName] = paramName
+    if exit:
+        break
+    goods.append((num, goodCard))
+    i += 1
+for items in goods:
+    print(items)
 
-while defaultDistance <= upgradeDistance:
-    defaultDistance = defaultDistance + (defaultDistance*0.1)
-    dayCount += 1
-
-print(f"Спортсмен достиг результата {defaultDistance:.2f} на {dayCount} день")
+goodsSort = {}
+for count, charact in goods:
+    for name, value in charact.items():
+        if name in goodsSort:
+            goodsSort[name].append(value)
+        else:
+            goodsSort[name] = [value]
+for itemParametr, names in goodsSort.items():
+    print(itemParametr, names)
